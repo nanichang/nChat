067eabaa94c1ec4ba785dd4da993779de7767ba5
nanichang <nanipaul68@gmail.com>
2017-08-10 18:24:39 +0100

    laravel-mix error fixed

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -10,8 +10,8 @@ window._ = require('lodash');
 window.$ = window.jQuery = require('jquery');
 
 require('bootstrap-sass');
-// require('noty');
-window.noty = require('noty');
+require('noty');
+// window.noty = require('noty');
 
 /**
  * Vue is a modern JavaScript library for building interactive web interfaces
